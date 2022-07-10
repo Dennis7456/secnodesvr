678c0c148e0ebd982e6dc5bbd9c17d731a84f4c5
webmasters2019 <lively222@live.com>
2022-07-10 07:09:44 +0300

    cloudinary module

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const { json } = require('express');
 const express = require('express');
 const app = express();
+const cloudinary = require('cloudinary').v2
+require('dotenv').config();
+const bodyParser = require('body-parser');
 
-app.use((request, response) => {
+//body parser config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//cloudinary config
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET 
+  });
+
+app.get('/', (request, response) => {
     response.json({message: 'Hey! This is your server response.'});
 });
+
+//image upload API
+app.post('/upload-image', (request, response) => {
+    //collected image from user
+    const data = {
+        image: request.body.image,
+    }
+
+    //upload image here
+    cloudinary.uploader.upload(data.image)
+    .then((result) => {
+        response.status(200).send({
+            message: 'success',
+            result,
+        });
+    }).catch((error) => {
+        response.status(500).send({
+            message: 'failure',
+            error,
+        });
+    });
+});
+
 module.exports = app;
